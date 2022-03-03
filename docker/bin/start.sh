ea61739c19a00ce71bbd5a2c174b6a941fd906fa
Noah Jahn <najahn19@gmail.com>
2022-03-03 15:38:07 -0500

    Temporarily remove quiet flag from pg_isready check

--- docker/bin/start.sh
+++ docker/bin/start.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # Nginx doesn't support environment variables in their configurations,
-# so this replaces any ${PORT} defined in the default.conf.template file 
-# and creates a conf for nginx to read at runtime
+# so this replaces any specified environment varaible defined in the 
+# default.conf.template file and creates a conf for nginx to read at runtime
 echo "Running envsubst on nginx template"
 envsubst '$PORT $NGINX_PROXY_SEND_TIMEOUT $NGINX_PROXY_READ_TIMEOUT $NGINX_FASTCGI_SEND_TIMEOUT $NGINX_FASTCGI_READ_TIMEOUT' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
 echo "Waiting for database to be ready..."
-until pg_isready --quiet --host=$DB_SERVER --port=$DB_PORT --dbname=$DB_DATABASE --username=$DB_USER
+until pg_isready --host=$DB_SERVER --port=$DB_PORT --dbname=$DB_DATABASE --username=$DB_USER
 do
     echo -ne "Waiting for database to be ready..."\\r
     sleep 2
